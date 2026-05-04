bf9596b5aec176d2fc6007a1975efb139eeb9e44
CodingManoj <mvp2612@hotmail.com>
2026-05-03 21:35:50 -0400

    Bash Basics

--- 02-print.sh
+++ 02-print.sh
@@ -4,3 +4,9 @@ echo hai
 echo hello
 echo welcome
 echo devsecops
+
+# Special Characters 
+# \n: Moves the cursor to the next line
+# \t: Moves the cursor to the next tab
+
+echo "hai \n hello \n welcome \n devsecops"
