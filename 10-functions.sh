34e153fd1bcb28240bf5c162141d7146e455f6e4
CodingManoj <mvp2612@hotmail.com>
2026-05-04 21:31:02 -0400

    Bash Basics

--- 10-functions.sh
+++ 10-functions.sh
@@ -10,3 +10,8 @@
 # Just by running "type command" shows you whether it's a Shell builting command, or alias or binary command
 # How to write an alias for a set of commands that we are executing ?
 # alias gp="git pull", from then you can run gp instead of "git pull"
+# alias commands are temporary, the moment you sign off, aliases will be taken down and when you login it back they don't work.
+
+# How can we make a specific alias command active all the time ?
+# /home/user/.bash_profile
+# Everytime the user signins in that will be executed.
