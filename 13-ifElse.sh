eff82223575711088edaedf574925fa2734b951f
CodingManoj <mvp2612@hotmail.com>
2026-05-04 22:04:41 -0400

    Bash Basics

--- 13-ifElse.sh
+++ 13-ifElse.sh
@@ -6,8 +6,23 @@ ACTION="$1"
 
 # == operator is used for strings
 # -eq operator is used for numbers 
+
+# Demo On IF Condition
+# if [ "$ACTION" == "start" ]; then 
+
+#     echo "Action is start"
+
+# fi
+
+
+# Demo On IF ELSE Condition;
+
 if [ "$ACTION" == "start" ]; then 
 
     echo "Action is start"
 
-fi 
+else
+
+    echo "Only approved value is start"
+
+fi
