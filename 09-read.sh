dbb8f56faa664c67046b90833810ab6092585048
CodingManoj <mvp2612@hotmail.com>
2026-05-04 21:21:57 -0400

    Bash Basics

--- 09-read.sh
+++ 09-read.sh
@@ -5,3 +5,6 @@ echo "This script will take the input from the user by interrupting the executio
 read -p "Name of the user executing the script:" NAME
 
 echo "User who executed the script is $NAME"
+
+echo "Above is the user who executed the script"
+echo "Name of the script is $0"
